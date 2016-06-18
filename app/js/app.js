3a81c3e7073a22d692dabaca1aaac830d2171379
netecodes <dotneko@gmail.com>
2016-06-18 12:29:15 +0200

    add photo grid  style and wworking input

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,15 @@
 //app.js
 $(document).ready( function() {
-
+	
 	$("#button").click(function() {
-		var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=APIKEYHERE&tags=london&safe_search=1&per_page=20";
+		var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=YOURAPIHERE&tags=";
+		var tag = $('input').val() || "berlin";
 		var src;
-		$.getJSON(url + "&format=json&jsoncallback=?", function(data){
+		$.getJSON(url + tag + "&format=json&jsoncallback=?", function(data){
     	$.each(data.photos.photo, function(i,item){
-    		console.log(item);
-        	src = "https://farm"+ item.farm + ".staticflickr.com/" + item.server + "/" + item.id + "_" + item.secret + "_m.jpg";
-        	$("<img/>").attr("src", src).appendTo("#images");
-        if ( i == 3 ) return false;
+        	src = "https://farm"+ item.farm + ".staticflickr.com/" + item.server + "/" + item.id + "_" + item.secret + ".jpg";
+        	$("#img-" + i).find("img").attr("src", src);
+       	 	if ( i == 9 ) return false;
     		});
 		});
 	});
